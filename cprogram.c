02ee9f64b7d671f28c172969ffffa9f32e33f730
Ubuntu <ubuntu@ip-172-31-5-213.ec2.internal>
2019-09-09 09:03:53 +0000

    I am pushing all files into git

---
+++ cprogram.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+int main() {
+printf("hello!!! I ama C program");
+return 0
+}
---
+++ hellojava.java
@@ -0,0 +1,6 @@
+class hellojava {
+public statis void main(String args[])
+{
+System.out.Println("Hello!!!I am a Java Program");
+}
+}
